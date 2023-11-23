c31dba47c765b9f83e479643c2ac0cf785d79b21
Shahrirar Rahman Shaon <shaon463@gmail.com>
2023-11-23 17:15:44 +0600

    Initial Commit

---
+++ BS23-iOSPractical/BS23_iOSPracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BS23_iOSPracticalApp.swift
+//  BS23-iOSPractical
+//
+//  Created by Sergio shaon on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BS23_iOSPracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
